86a5315aecaff10d5b5dfaab3fb1440e217c97cc
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-01-25 05:46:08 -0800

    git init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 require('dotenv').config();
 
 const express = require('express');
-
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine','ejs');
+app.set('views','src/views');
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res, next) => {
-  const userName = process.env.USER_NAME || 'World';
-  res.send(`<h1>Hello ${userName}!</h1>`)
+  // const userName = process.env.USER_NAME || 'World';
+  // res.send(`<h1>Hello ${userName}!</h1>`)
+  res.render('login');
 });
+
+
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
